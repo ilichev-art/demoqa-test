a4c7ca03ba6a57b3a19ef900bf0866500ce0148e
Artem Ilichev <artem.ilichev@list.ru>
2024-05-11 19:17:01 +0300

    add base url, browser size, load strategy, headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,11 @@ from selenium import webdriver
 @pytest.fixture(scope='module', autouse=True)
 def manage_browser():
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.window_height = 1080
+    browser.config.window_width = 1366
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless=new')
+    options.page_load_strategy = 'eager'
+    options.add_argument('--headless=new')
     browser.config.driver_options = options
 
     yield
