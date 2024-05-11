fb3cfa17799be822b76bacd94f7d0643a72ddd90
Artem Ilichev <artem.ilichev@list.ru>
2024-05-11 11:40:48 +0300

    Промежуточный результат

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='module', autouse=True)
+def manage_browser():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless=new')
+    browser.config.driver_options = options
+
+    yield
+
+    browser.quit()
---
+++ tests/test_demoqa.py
@@ -0,0 +1,21 @@
+from selene import browser, be, have
+
+
+def test_form_autocomplete():
+    browser.open('/')
+    browser.element('#firstName').should(be.blank).type('Artem')
+    browser.element('#lastName').should(be.blank).type('Ilichev')
+    browser.element('#userEmail').should(be.blank).type('test@mail.ru')
+    browser.all('#genterWrapper>div>div').should(have.exact_texts('Male', 'Female', 'Other'))
+    browser.all('.col-md-9.col-sm-12>div').element_by(have.exact_text('Other')).click()
+    browser.element('#userNumber').type('90957485747')
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.all('.react-datepicker__year-select>option').element_by(have.exact_text('1989')).click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.all('.react-datepicker__month-select>option').element_by(have.exact_text('March')).click()
+    browser.element('.react-datepicker__day.react-datepicker__day--021').click()
+    browser.element('.css-2b097c-container').click()
+
+
+
