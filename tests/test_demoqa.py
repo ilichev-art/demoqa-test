5f4a935fda3e8370c7c2f46aa16aa90ee1683e9e
Artem Ilichev <artem.ilichev@list.ru>
2024-05-12 13:39:17 +0300

    fix

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,36 +1,49 @@
-from selene import browser, be, have
+from selene import browser, have
 import os
 
 
 def test_form_autocomplete():
-    browser.open('/')
+
+    # Открытие раздела с формой ввода данных
+    browser.open('/automation-practice-form')
     # Ввод данных
-    browser.element('#firstName').should(be.blank).type('Mahatma')
-    browser.element('#lastName').should(be.blank).type('Gandhi')
-    browser.element('#userEmail').should(be.blank).type('test@mail.ru')
-    browser.all('#genterWrapper>div>div').should(have.exact_texts('Male', 'Female', 'Other'))
-    browser.all('.col-md-9.col-sm-12>div').element_by(have.exact_text('Other')).click()
+    # Ввод FistName и LastName
+    browser.element('#firstName').type('Mahatma')
+    browser.element('#lastName').type('Gandhi')
+    # Ввод почты
+    browser.element('#userEmail').type('test@mail.ru')
+    # Активация радиокнопки в блоке Gender
+    browser.all('.col-sm-12>div').element_by(have.exact_text('Other')).click()
+    # Ввод номера телефона
     browser.element('#userNumber').type('9095748574')
+    # Установка даты рождения
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__year-select').click()
     browser.all('.react-datepicker__year-select>option').element_by(have.exact_text('1989')).click()
     browser.element('.react-datepicker__month-select').click()
     browser.all('.react-datepicker__month-select>option').element_by(have.exact_text('March')).click()
     browser.element('.react-datepicker__day.react-datepicker__day--021').click()
-    browser.element('#subjectsInput').should(be.clickable).type('computer').press_tab()
-    browser.all('#hobbiesWrapper>div>div').should(have.exact_texts('Sports', 'Reading', 'Music'))
-    browser.element('.custom-control.custom-checkbox.custom-control-inline').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('/Users/a.ilichev/Downloads/1703075063565.jpeg'))
-    browser.element('#currentAddress').should(be.blank).type('Mytishchi')
+    # Выбор предмета
+    browser.element('#subjectsInput').type('computer').press_tab()
+    # Активация чек-бокса в разделе Hobbies
+    browser.element('//*[@for="hobbies-checkbox-1"]').click()
+    # Загрузка изображения
+    browser.element('#uploadPicture').send_keys(os.path.abspath('Pictures/1703075063565.jpeg'))
+    # Ввод адреса
+    browser.element('#currentAddress').type('Mytishchi')
+    # Выбор штата
+    # browser.element('#state').click()
     browser.element('#state').click()
-    browser.element('#state [class$=menu] [id$=option-2]').should(have.text('Haryana')).click()
+    browser.element('#state[class$=menu][id$=option-2]').should(have.text('Haryana')).click()
+    # Выбор города
     browser.element('#city').click()
-    browser.element('#city [class$=menu] [id$=option-0]').should(have.exact_text('Karnal')).click()
-    browser.element('#submit').should(be.clickable).click()
+    browser.element('#city[class$=menu][id$=option-0]').should(have.exact_text('Karnal')).click()
+    # Отправка данных
+    browser.element('#submit').click()
 
     # Проверка введенных данных
-    browser.element(('css selector', '.modal-content')).element(('css selector', 'table')).all(
-        ('css selector', 'tr')).all(('css selector', 'td'))[1::2].should(
+    browser.element('.modal-content').element('table').all(
+        'tr').all('td')[1::2].should(
         have.exact_texts(
             'Mahatma Gandhi',
             'test@mail.ru',
@@ -43,7 +56,6 @@ def test_form_autocomplete():
             'Mytishchi',
             'Haryana Karnal'
         ))
-
 
 
 
