4421b09c92881df7bdfe79a5702895828a7ae1ac
Artem Ilichev <artem.ilichev@list.ru>
2024-05-12 13:53:18 +0300

    fix

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -6,16 +6,22 @@ def test_form_autocomplete():
 
     # Открытие раздела с формой ввода данных
     browser.open('/automation-practice-form')
+
     # Ввод данных
     # Ввод FistName и LastName
     browser.element('#firstName').type('Mahatma')
     browser.element('#lastName').type('Gandhi')
+
     # Ввод почты
     browser.element('#userEmail').type('test@mail.ru')
+
     # Активация радиокнопки в блоке Gender
-    browser.all('.col-sm-12>div').element_by(have.exact_text('Other')).click()
+    # browser.all('.col-sm-12>div').element_by(have.exact_text('Other')).click()
+    browser.element('#genterWrapper').element('[for="gender-radio-3"]').click()
+
     # Ввод номера телефона
     browser.element('#userNumber').type('9095748574')
+
     # Установка даты рождения
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__year-select').click()
@@ -25,16 +31,20 @@ def test_form_autocomplete():
     browser.element('.react-datepicker__day.react-datepicker__day--021').click()
     # Выбор предмета
     browser.element('#subjectsInput').type('computer').press_tab()
+
     # Активация чек-бокса в разделе Hobbies
     browser.element('//*[@for="hobbies-checkbox-1"]').click()
+
     # Загрузка изображения
-    browser.element('#uploadPicture').send_keys(os.path.abspath('Pictures/1703075063565.jpeg'))
+    browser.element('#uploadPicture').send_keys(os.path.abspath('tests/Pictures/1703075063565.jpeg'))
+
     # Ввод адреса
     browser.element('#currentAddress').type('Mytishchi')
-    # Выбор штата
+
+    # Выбор штата и города
     browser.element('#state').click().element('#react-select-3-option-2').click()
-    # Выбор города
     browser.element('#city').click().element('#react-select-4-option-0').click()
+
     # Отправка данных
     browser.element('#submit').click()
 
