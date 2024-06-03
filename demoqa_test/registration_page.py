30021bb80ea2cccdfad5fbc042050a361ffd01d1
Artem Ilichev <artem.ilichev@list.ru>
2024-06-03 18:48:03 +0300

    reformat сode

--- demoqa_test/registration_page.py
+++ demoqa_test/registration_page.py
@@ -7,78 +7,88 @@ import tests
 
 class RegistrationPage:
     def __init__(self):
-        self.state = browser.element('#state')
-        self.city = browser.element('#city')
+        self.state = browser.element("#state")
+        self.city = browser.element("#city")
 
     def open(self):
-        browser.open('/automation-practice-form')
+        browser.open("/automation-practice-form")
 
     def fill_first_name(self, value):
-        browser.element('#firstName').type(value)
+        browser.element("#firstName").type(value)
         return self
 
     def fill_second_name(self, value):
-        browser.element('#lastName').type(value)
+        browser.element("#lastName").type(value)
         return self
 
     def fill_email(self, value):
-        browser.element('#userEmail').type(value)
+        browser.element("#userEmail").type(value)
         return self
 
     def fill_gender(self, value):
-        browser.all('[name=gender]').element_by(have.value(value)).element("..").click()
+        browser.all("[name=gender]").element_by(have.value(value)).element("..").click()
         return self
 
     def fill_phone(self, value):
-        browser.element('#userNumber').type(value)
+        browser.element("#userNumber").type(value)
         return self
 
     def fill_date_of_birth(self, year, month, day):
-        browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__year-select').click()
-        browser.all('.react-datepicker__year-select>option').element_by(have.exact_text(year)).click()
-        browser.element('.react-datepicker__month-select').click()
-        browser.all('.react-datepicker__month-select>option').element_by(have.exact_text(month)).click()
-        browser.element(f'.react-datepicker__day.react-datepicker__day--0{day}').click()
+        browser.element("#dateOfBirthInput").click()
+        browser.element(".react-datepicker__year-select").click()
+        browser.all(".react-datepicker__year-select>option").element_by(
+            have.exact_text(year)
+        ).click()
+        browser.element(".react-datepicker__month-select").click()
+        browser.all(".react-datepicker__month-select>option").element_by(
+            have.exact_text(month)
+        ).click()
+        browser.element(f".react-datepicker__day.react-datepicker__day--0{day}").click()
         return self
 
     def fill_subject(self, value):
-        browser.element('#subjectsInput').type(value).press_tab()
+        browser.element("#subjectsInput").type(value).press_tab()
         return self
 
     def upload_file(self, file_name):
-        browser.element('#uploadPicture').set_value(
-            str(Path(tests.__file__).parent.joinpath(f'pictures/{file_name}').absolute()))
+        browser.element("#uploadPicture").set_value(
+            str(
+                Path(tests.__file__).parent.joinpath(f"pictures/{file_name}").absolute()
+            )
+        )
         return self
 
     def fill_hobbies(self, value):
-        browser.all('.custom-checkbox').element_by(have.exact_text(value)).click()
+        browser.all(".custom-checkbox").element_by(have.exact_text(value)).click()
         return self
 
     def fill_current_address(self, value):
-        browser.element('#currentAddress').type(value)
+        browser.element("#currentAddress").type(value)
         return self
 
     def fill_state_and_city(self, state, city):
         self.state.perform(command.js.scroll_into_view)
         self.state.click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(state)).click()
+        browser.all("[id^=react-select][id*=option]").element_by(
+            have.exact_text(state)
+        ).click()
         self.city.perform(command.js.scroll_into_view)
         self.city.click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(city)).click()
+        browser.all("[id^=react-select][id*=option]").element_by(
+            have.exact_text(city)
+        ).click()
         return self
 
     def submit(self):
-        browser.element('#submit').click()
+        browser.element("#submit").click()
         return self
 
-    def registered_user_with(self, full_name, email, gender, phone, date_of_birth, subject, hobby, photo, address,
-                             state):
-        browser.element('.modal-content').element('table').all(
-            'tr').all('td').even.should(have.exact_texts(
-            full_name,
+    def registered_user_with(self, full_name, email, gender, phone, date_of_birth, subject, hobby, photo, address, state):
+        browser.element(".modal-content").element("table").all("tr").all(
+            "td"
+        ).even.should(
+            have.exact_texts(
+                full_name,
                 email,
                 gender,
                 phone,
@@ -87,6 +97,6 @@ class RegistrationPage:
                 hobby,
                 photo,
                 address,
-                state
+                state,
             )
         )
--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -1,4 +1,3 @@
-
 from demoqa_test.registration_page import RegistrationPage
 
 
@@ -6,32 +5,29 @@ def test_form_autocomplete():
     registration_page = RegistrationPage()
     registration_page.open()
     (
-        registration_page
-        .fill_first_name('Mahatma')
-        .fill_second_name('Gandhi')
-        .fill_email('test@mail.ru')
-        .fill_gender('Other')
-        .fill_phone('9095748574')
-        .fill_date_of_birth('1989', 'March', '21')
-        .fill_subject('computer')
-        .fill_hobbies('Sports')
-        .upload_file('1703075063565.jpeg')
-        .fill_current_address('Mytishchi')
-        .fill_state_and_city('Haryana', 'Karnal')
-
+        registration_page.fill_first_name("Mahatma")
+        .fill_second_name("Gandhi")
+        .fill_email("test@mail.ru")
+        .fill_gender("Other")
+        .fill_phone("9095748574")
+        .fill_date_of_birth("1989", "March", "21")
+        .fill_subject("computer")
+        .fill_hobbies("Sports")
+        .upload_file("1703075063565.jpeg")
+        .fill_current_address("Mytishchi")
+        .fill_state_and_city("Haryana", "Karnal")
         .submit()
-
         .registered_user_with(
-            'Mahatma Gandhi',
-            'test@mail.ru',
-             'Other',
-             '9095748574',
-             '21 March,1989',
-             'Computer Science',
-              'Sports',
-             '1703075063565.jpeg',
-             'Mytishchi',
-             'Haryana Karnal'
+            "Mahatma Gandhi",
+            "test@mail.ru",
+            "Other",
+            "9095748574",
+            "21 March,1989",
+            "Computer Science",
+            "Sports",
+            "1703075063565.jpeg",
+            "Mytishchi",
+            "Haryana Karnal",
         )
     )
 
@@ -75,8 +71,3 @@ def test_form_autocomplete():
     #     'Mytishchi',
     #     'Haryana Karnal'
     # )
-
-
-
-
-
