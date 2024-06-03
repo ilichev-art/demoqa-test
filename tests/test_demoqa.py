74669ea9f95a4e6d715a30fe934d7884327c0b38
Artem Ilichev <artem.ilichev@list.ru>
2024-06-03 19:10:08 +0300

    delete comment

--- tests/test_demoqa.py
+++ tests/test_demoqa.py
@@ -31,43 +31,4 @@ def test_form_autocomplete():
         )
     )
 
-    # registration_page.open()
-    #
-    # # WHEN
-    # registration_page.fill_first_name('Mahatma')
-    # registration_page.fill_second_name('Gandhi')
-    #
-    # registration_page.fill_email('test@mail.ru')
-    #
-    # registration_page.fill_gender('Other')
-    #
-    # registration_page.fill_phone('9095748574')
-    #
-    # registration_page.fill_date_of_birth('1989', 'March', '21')
-    #
-    # registration_page.fill_subject('computer')
-    #
-    # registration_page.set_hobbies('Sports')
-    #
-    # registration_page.upload_file('1703075063565.jpeg')
-    #
-    # registration_page.fill_current_address('Mytishchi')
-    #
-    # registration_page.fill_state_and_city('Haryana', 'Karnal')
-    #
-    # registration_page.submit()
-    #
-    # #THEN
-    # # Проверка введенных данных
-    # registration_page.registered_user_with(
-    #     'Mahatma Gandhi',
-    #     'test@mail.ru',
-    #     'Other',
-    #     '9095748574',
-    #     '21 March,1989',
-    #     'Computer Science',
-    #     'Sports',
-    #     '1703075063565.jpeg',
-    #     'Mytishchi',
-    #     'Haryana Karnal'
-    # )
+
