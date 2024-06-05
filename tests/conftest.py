e82099071041d0ee7658cf16a3a3317b2230eb9b
Artem Ilichev <artem.ilichev@list.ru>
2024-06-05 20:52:43 +0300

    add conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,19 @@
-# import pytest
-# from selene import browser
-# from selenium import webdriver
-#
-#
-# @pytest.fixture(scope='module', autouse=True)
-# def manage_browser():
-#     browser.config.base_url = 'https://demoqa.com'
-#     browser.config.window_height = 1080
-#     browser.config.window_width = 1366
-#     options = webdriver.ChromeOptions()
-#     options.page_load_strategy = 'eager'
-#     options.add_argument('--headless=new')
-#     browser.config.driver_options = options
-#
-#     yield
-#
-#     browser.quit()
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='module', autouse=True)
+def manage_browser():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_height = 1080
+    browser.config.window_width = 1366
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = 'eager'
+    options.add_argument('--headless=new')
+    browser.config.driver_options = options
+
+    yield
+
+    browser.quit()
 
