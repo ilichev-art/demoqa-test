ccb0436f3644f19b58993eb12ce44752271fdfbf
Artem Ilichev <artem.ilichev@list.ru>
2024-06-06 13:00:12 +0300

    add attach.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,72 @@
 import pytest
-from selene import browser
+from dotenv import load_dotenv
+import os
+from selene.support.shared import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils import attach
 
 
-@pytest.fixture(scope='module', autouse=True)
-def manage_browser():
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = 1080
-    browser.config.window_width = 1366
-    options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'eager'
-    options.add_argument('--headless=new')
-    browser.config.driver_options = options
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
-    yield
+
+@pytest.fixture(scope="function")
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {"enableVideo": True, "enableVNC": True},
+    }
+
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options,
+    )
+    browser.config.driver = driver
+
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.window_width = int(os.getenv("selene.window_width", 1920))
+    browser.config.window_height = int(os.getenv("selene.window_heigh", 1200))
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
+
+
+
+
+
+
+
+# import pytest
+# from selene import browser
+# from selenium import webdriver
+#
+#
+# @pytest.fixture(scope='module', autouse=True)
+# def manage_browser():
+#     browser.config.base_url = 'https://demoqa.com'
+#     browser.config.window_height = 1080
+#     browser.config.window_width = 1366
+#     options = webdriver.ChromeOptions()
+#     options.page_load_strategy = 'eager'
+#     options.add_argument('--headless=new')
+#     browser.config.driver_options = options
+#
+#     yield
+#
+#     browser.quit()
 
